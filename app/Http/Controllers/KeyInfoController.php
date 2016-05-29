24f189331386ad887c002ce3c8e484f645497198
Kazuhisa Suematsu <suematsu@mediawars.ne.jp>
2016-05-29 21:44:48 +0900

    テストでの修正

---
+++ app/Http/Controllers/KeyInfoController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+
+class KeyInfoController extends Controller
+{
+    //
+    public function index()
+    {
+        //
+	return view('keyinfo.index');
+    }
+    public function create()
+    {
+        //
+    }
+    public function edit()
+    {
+        //
+    }
+    public function delete()
+    {
+        //
+    }
+
+
+}
