4d4457f56ec545ab83377866784fd095e3dba4e6
Kazuhisa Suematsu <suematsu@uss-r.jp>
2016-05-29 21:45:35 +0900

    jsとcssをpublicに設置

---
+++ public/scripts/app.js
@@ -0,0 +1,20 @@
+﻿$(function () {
+    //date picker
+    $('.cal').datepicker({
+        format: 'yyyy/mm/dd',
+        language: 'ja',
+        autoclose: true,
+        todayHighlight: true
+    });
+
+    $('.cal-yyyymm').datepicker({
+        format: 'yyyy/mm',
+        language: 'ja',
+        minViewMode: 'months',
+        autoclose: true
+    });
+
+});
+
+
+
