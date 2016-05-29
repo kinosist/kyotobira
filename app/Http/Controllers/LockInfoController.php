a393ac79713220a0036ec4a0d4592e47b68f74e7
Kazuhisa Suematsu <suematsu@uss-r.jp>
2016-05-30 07:33:44 +0900

    lockinfoをいったん表示可能に

--- app/Http/Controllers/LockInfoController.php
+++ app/Http/Controllers/LockInfoController.php
@@ -12,18 +12,21 @@ class LockInfoController extends Controller
     public function index()
     {
         //
-	return view('keyinfo.index');
+	return view('lockinfo.index');
     }
     public function create()
     {
         //
+	return view('lockinfo.create');
     }
     public function edit()
     {
         //
+	return view('lockinfo.edit');
     }
     public function delete()
     {
         //
+	return view('lockinfo.index');
     }
 }
